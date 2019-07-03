ff398bc48858577ca7073b7aaa2c0d3795cfadc5
Memou <equaco@gmail.com>
2019-07-03 01:42:40 -0400

    Added dynamic alert for empty querying,clear button,setup router and about page

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
- 
-const Navbar = ({icon,title}) => {
+import {Link} from 'react-router-dom'
+
+const Navbar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+        <Link to='/about'>About</Link>
+
+        </li>
+      </ul>
+
     </nav>
   );
 };
